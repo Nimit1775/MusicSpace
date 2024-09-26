fa4e3bf9af9626a100056063baaa6a1b410b2378
nimit sodhani <nimitsodhani1@gmail.com>
2024-09-26 21:50:08 +0530

    cant make it real time

--- backend/index.ts
+++ backend/index.ts
@@ -37,6 +37,39 @@ io.on('connection', (socket) => {
     socket.leave(spaceId);
   });
 });
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('join_space', (spaceId) => {
+    socket.join(spaceId);
+    console.log(`User joined space: ${spaceId}`);
+  });
+
+  socket.on('leave_space', (spaceId) => {
+    socket.leave(spaceId);
+    console.log(`User left space: ${spaceId}`);
+  });
+
+  // Emit when a video is added to the queue
+  socket.on('add_to_queue', (spaceId, video) => {
+    io.to(spaceId).emit('update_queue', video);
+  });
+
+  // Emit when a video is removed from the queue
+  socket.on('remove_from_queue', (spaceId, queueId) => {
+    io.to(spaceId).emit('queue_removed', queueId);
+  });
+
+  // Emit when a video ends
+  socket.on('video_ended', (spaceId, queueId) => {
+    io.to(spaceId).emit('video_ended', queueId);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
